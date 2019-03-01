359d4941d467f5041f5306a29941bce8a1524381
Jackal Lee <jackcakal@gmail.com>
2019-03-01 21:35:36 +0800

    Download MVC template with auth

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace fans.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
